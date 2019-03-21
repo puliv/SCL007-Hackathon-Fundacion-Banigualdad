5103e227d8d72343497fcc42332b53fbd4118826
LeslieSaVa <leslie.sabattin@usach.cl>
2019-03-21 10:18:05 -0300

    pantalla tinder casi falta carrousel

--- src/components/main3.js
+++ src/components/main3.js
@@ -11,25 +11,25 @@ function Main3 () {
                     <p className="title">Denim Jacket</p>
                     <p className="value">$15000</p>
                 </div>
-                <div class="carousel carousel-slider center">
-                    <div class="carousel-fixed-item center">
-                        <a class="btn-floating btn-large waves-effect"><i class="material-icons">phone</i></a>
+                <div className="carousel carousel-slider center">
+                    <div className="carousel-fixed-item center">
+                        <a className="btn-floating btn-large waves-effect"><i className="material-icons">phone</i></a>
                     </div>
-                    <div class="carousel-item red white-text" href="#one!">
+                    <div className="carousel-item red white-text" href="#one!">
                         <h2>First Panel</h2>
-                        <p class="white-text">This is your first panel</p>
+                        <p className="white-text">This is your first panel</p>
                     </div>
-                    <div class="carousel-item amber white-text" href="#two!">
+                    <div className="carousel-item amber white-text" href="#two!">
                         <h2>Second Panel</h2>
-                        <p class="white-text">This is your second panel</p>
+                        <p className="white-text">This is your second panel</p>
                     </div>
-                    <div class="carousel-item green white-text" href="#three!">
+                    <div className="carousel-item green white-text" href="#three!">
                         <h2>Third Panel</h2>
-                        <p class="white-text">This is your third panel</p>
+                        <p className="white-text">This is your third panel</p>
                     </div>
-                    <div class="carousel-item blue white-text" href="#four!">
+                    <div className="carousel-item blue white-text" href="#four!">
                         <h2>Fourth Panel</h2>
-                        <p class="white-text">This is your fourth panel</p>
+                        <p className="white-text">This is your fourth panel</p>
                     </div>
                 </div>
                 <div>
