95f02bdb017345edb096fee138465a9d42a39b09
Marce Arratia <marratiaf@hotmail.com>
2019-03-21 22:42:29 -0300

    actualizando json de nuevo

--- src/Component/SearchRegion.js
+++ src/Component/SearchRegion.js
@@ -24,19 +24,19 @@ class SearchRegion extends Component {
       <div >
      <select id="selectRegion" onChange={event => this.filterRegion({userSelect:event.target.value})}>
                     <option value="0" defaultValue>Seleccione Región</option>
-                    <option value="Tarapacá">Región de Tarapacá</option>
-                    <option value="Antofagasta">Región de Antofagasta</option>
-                    <option value="Atacama">Región de Atacama</option>
-                    <option value="Coquimbo">Región de Coquimbo</option>
-                    <option value="Valparaíso">Región de Valparaíso</option>
-                    <option value="Libertador">Región del Libertador Gral. Bernardo O'Higgins</option>
-                    <option value="Maule">Región del Maule</option>
-                    <option value="Ñuble">Región del Ñuble</option>
-                    <option value="Bíobío">Región del Bíobío</option>
-                    <option value="La Araucanía">Región De La Araucanía</option>
-                    <option value="Los Ríos">Región de Los Ríos</option>
-                    <option value="Los Lagos">Región de Los Lagos</option>
-                    <option value="Region Metropolitana">Región Metropolitana</option>
+                    <option value="Región de Tarapacá">Región de Tarapacá</option>
+                    <option value="Región de Antofagasta">Región de Antofagasta</option>
+                    <option value="Región de Atacama">Región de Atacama</option>
+                    <option value="Región de Coquimbo">Región de Coquimbo</option>
+                    <option value="Región de Valparaíso">Región de Valparaíso</option>
+                    <option value="Región de Ohiggins">Región de O'Higgins</option>
+                    <option value="Región de Maule">Región del Maule</option>
+                    <option value="Región de Ñuble">Región del Ñuble</option>
+                    <option value="Región de Bíobío">Región del Bíobío</option>
+                    <option value="Región de La Araucanía">Región De La Araucanía</option>
+                    <option value="Región de Los Ríos">Región de Los Ríos</option>
+                    <option value="Región de Los Lagos">Región de Los Lagos</option>
+                    <option value="Región Metropolitana">Región Metropolitana</option>
                     </select>
       </div>
     );
